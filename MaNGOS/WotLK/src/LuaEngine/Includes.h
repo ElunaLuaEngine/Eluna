75b779185915ea6ff381e3ebf40eea1ece58dfbb
Easelm <easelm@live.com>
2013-11-13 15:34:21 -0500

    UPDATED: MaNGOS and TrinityCore
    ADDED: MaNGOS Two (WotLK) to our project. It has limited functionality so bare with us when we develop this.
    
    UPDATED: README files

---
+++ MaNGOS/WotLK/src/LuaEngine/Includes.h
@@ -0,0 +1,34 @@
+/* Copyright (C) 2010 - 2013 Eluna Lua Engine <http://emudevs.com/>
+* This program is free software licensed under GPL version 3
+* Please see the included DOCS/LICENSE.TXT for more information 
+*/
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include "AccountMgr.h"
+#include "ArenaTeam.h"
+#include "AuctionHouseMgr.h"
+#include "Chat.h"
+#include "Channel.h"
+#include "Creature.h"
+#include "Group.h"
+#include "Guild.h"
+#include "GuildMgr.h"
+#include "Language.h"
+#include "Map.h"
+#include "MapManager.h"
+#include "ObjectMgr.h"
+#include "Pet.h"
+#include "QuestDef.h"
+#include "GossipDef.h"
+#include "ScriptMgr.h"
+#include "CreatureAI.h"
+#include "SharedDefines.h"
+#include "SystemConfig.h"
+#include "Vehicle.h"
+#include "ItemPrototype.h"
+#include "Spell.h"
+#include "GameObject.h"
+#include "Weather.h"
+
+#endif
