ac390265455b9fca8de2a097cb0c033eae9f1b93
Rochet2 <tqbattlenet@gmail.com>
2014-03-26 22:41:02 +0200

    Eluna fix classic

--- LuaEngine.h
+++ LuaEngine.h
@@ -97,9 +97,9 @@ typedef std::set<std::string> LoadedScripts;
 typedef TemporarySummon TempSummon;
 #ifndef CLASSIC
 #define PLAYER_FIELD_LIFETIME_HONORABLE_KILLS   PLAYER_FIELD_LIFETIME_HONORBALE_KILLS
+typedef VehicleInfo Vehicle;
 #endif
 #define MAX_TALENT_SPECS        MAX_TALENT_SPEC_COUNT
-typedef VehicleInfo Vehicle;
 #define GUID_ENPART(guid)       ObjectGuid(guid).GetEntry()
 #define GUID_LOPART(guid)       ObjectGuid(guid).GetCounter()
 #define GUID_HIPART(guid)       ObjectGuid(guid).GetHigh()
