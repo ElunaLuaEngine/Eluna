6f6a9bb1495543c210d072b995ecb8edaf1853cb
Rochet2 <rochet2@post.com>
2014-04-07 10:32:50 +0300

    Update GroupMethods.h

--- GroupMethods.h
+++ GroupMethods.h
@@ -231,7 +231,7 @@ namespace LuaGroup
         if (icon >= TARGETICONCOUNT)
             return luaL_argerror(L, 2, "valid target icon expected");
 
-#if (defined(TBC) || defined(TBC))
+#if (defined(CLASSIC) || defined(TBC))
         group->SetTargetIcon(icon, ObjectGuid(target));
 #else
         group->SetTargetIcon(icon, ObjectGuid(setter), ObjectGuid(target));
