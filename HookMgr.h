92c7c25ef6a0620cede58a0e9a0ca3723bc7dc96
Rochet2 <rochet2@post.com>
2014-09-15 18:59:37 +0300

    Fix comment typo

--- HookMgr.h
+++ HookMgr.h
@@ -247,7 +247,7 @@ namespace HookMgr
         GAMEOBJECT_EVENT_ON_DAMAGED                     = 8,    // (event, go, player)
         GAMEOBJECT_EVENT_ON_LOOT_STATE_CHANGE           = 9,    // (event, go, state)
         GAMEOBJECT_EVENT_ON_GO_STATE_CHANGED            = 10,   // (event, go, state)
-        // GAMEOBJECT_EVENT_ON_QUEST_COMPLETE           = 11,   // (event, player, go, quest) Decapreted, use GAMEOBJECT_EVENT_ON_QUEST_COMPLETE
+        // GAMEOBJECT_EVENT_ON_QUEST_COMPLETE           = 11,   // (event, player, go, quest) Decapreted, use GAMEOBJECT_EVENT_ON_QUEST_REWARD
         GAMEOBJECT_EVENT_ON_ADD                         = 12,   // (event, gameobject)
         GAMEOBJECT_EVENT_ON_REMOVE                      = 13,   // (event, gameobject)
         GAMEOBJECT_EVENT_COUNT
