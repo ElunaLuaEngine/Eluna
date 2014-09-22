774df595a6ae50c4b3db9e7b2cca8682ef78fc5b
emudevs <easelm@live.com>
2014-09-22 01:08:10 -0400

    Corrected typo

--- CreatureMethods.h
+++ CreatureMethods.h
@@ -637,8 +637,8 @@ namespace LuaCreature
      * <pre>
      * enum SelectAggroTarget
      * {
-     *     SELECT_TARGET_RANDOM = 0, //Just selects a random target
-     *     SELECT_TARGET_TOPAGGRO, //Selects targes from top aggro to bottom
+     *     SELECT_TARGET_RANDOM = 0,  //Just selects a random target
+     *     SELECT_TARGET_TOPAGGRO,    //Selects targets from top aggro to bottom
      *     SELECT_TARGET_BOTTOMAGGRO, //Selects targets from bottom aggro to top
      *     SELECT_TARGET_NEAREST,
      *     SELECT_TARGET_FARTHEST
