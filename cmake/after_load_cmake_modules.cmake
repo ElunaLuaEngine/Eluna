e27b9e0d892d1627b1bccc16f7ceeca9f9bcbe61
Kargatum <dowlandtop@yandex.com>
2019-04-03 12:32:07 +0700

    Cmake: Rewrite build and use inherited dependencies (#12)

--- cmake/after_load_cmake_modules.cmake
+++ cmake/after_load_cmake_modules.cmake
@@ -1,7 +1,9 @@
 add_subdirectory(${CMAKE_MOD_ELUNA_ENGINE_DIR}/lualib)
+
 add_definitions(-DELUNA)
 add_definitions(-DAZEROTHCORE)
 add_definitions(-DWOTLK)
+
 CU_ADD_HOOK(BEFORE_GAME_LIBRARY "${CMAKE_MOD_ELUNA_ENGINE_DIR}/cmake/before_gs_install.cmake")
 CU_ADD_HOOK(AFTER_GAME_LIBRARY "${CMAKE_MOD_ELUNA_ENGINE_DIR}/cmake/after_gs_install.cmake")
 CU_ADD_HOOK(BEFORE_SCRIPTS_LIBRARY "${CMAKE_MOD_ELUNA_ENGINE_DIR}/cmake/before_script_install.cmake")
@@ -10,4 +12,3 @@ CU_ADD_HOOK(AFTER_WORLDSERVER_CMAKE "${CMAKE_MOD_ELUNA_ENGINE_DIR}/cmake/after_w
 AC_ADD_CONFIG_FILE("${CMAKE_MOD_ELUNA_ENGINE_DIR}/conf/mod_LuaEngine.conf.dist")
 
 message("** [Eluna Module] LuaEngine is enable!")
-
