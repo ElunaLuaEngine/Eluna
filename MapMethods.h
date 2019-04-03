6f28bd9c6459a38521bff7f10e3736a2450b6561
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-04-03 09:17:24 +0000

    fix some compiler warnings about unused variables

--- MapMethods.h
+++ MapMethods.h
@@ -263,6 +263,7 @@ namespace LuaMap
      */
     int SetWeather(lua_State* L, Map* map)
     {
+        (void)map; // ensure that the variable is referenced in order to pass compiler checks
         uint32 zoneId = Eluna::CHECKVAL<uint32>(L, 2);
         uint32 weatherType = Eluna::CHECKVAL<uint32>(L, 3);
         float grade = Eluna::CHECKVAL<float>(L, 4);
--- PlayerMethods.h
+++ PlayerMethods.h
@@ -2474,6 +2474,7 @@ namespace LuaPlayer
      */
     int LeaveBattleground(lua_State* L, Player* player)
     {
+        (void)L; // ensure that the variable is referenced in order to pass compiler checks
 #ifndef AZEROTHCORE
         bool teleToEntryPoint = Eluna::CHECKVAL<bool>(L, 2, true);
         player->LeaveBattleground(teleToEntryPoint);
@@ -3493,6 +3494,7 @@ namespace LuaPlayer
     {
         uint32 category = Eluna::CHECKVAL<uint32>(L, 2);
         bool update = Eluna::CHECKVAL<bool>(L, 3, true);
+        (void)update; // ensure that the variable is referenced in order to pass compiler checks
 
 #ifdef TRINITY
         player->GetSpellHistory()->ResetCooldowns([category](SpellHistory::CooldownStorageType::iterator itr) -> bool
