df544ec5552f577d72c9e5656fda59e32a5ad9a1
Grumbo <blackwolfsden@hotmail.com>
2020-05-29 20:49:41 -0600

    Update GlobalMethods.h

--- GlobalMethods.h
+++ GlobalMethods.h
@@ -103,6 +103,30 @@ namespace LuaGlobalFunctions
     }
 #endif
 
+      /**
+       * Returns emulator .conf RealmID
+       *
+       * - for MaNGOS returns the realmID as it is stored in the core.
+       * - for TrinityCore returns the realmID as it is set in the conf file.
+       * - Grumbo Was HERE !!
+       * @return uint32 realm ID
+       */
+  #if defined(MANGOS)
+      int GetRealmID(lua_State* L)
+      {
+          Eluna::Push(L, eWorld->GetRealmID());
+          return 1;
+      }
+	#endif
+
+  #if defined(TRINITY)
+      int GetRealmID(lua_State* L)
+      {
+          Eluna::Push(L, sConfigMgr->GetIntDefault("RealmID", 1));
+          return 1;
+      }
+  #endif
+    
     /**
      * Returns [Quest] template
      *
