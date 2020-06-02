3419e76486752f9fd72fdf390b45485ab4ab9caa
ayase <137056643@qq.com>
2020-06-02 22:49:42 +0800

    杂项修改

--- cmake/after_gs_install.cmake
+++ cmake/after_gs_install.cmake
@@ -16,19 +16,14 @@ target_link_libraries(game
     lualib)
 
 if( WIN32 )
-  if ( MSVC )
-    add_custom_command(TARGET game
-    POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_BINARY_DIR}/bin/$(ConfigurationName)/lua_scripts/extensions/"
-    COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_MOD_ELUNA_ENGINE_DIR}/LuaEngine/extensions" "${CMAKE_BINARY_DIR}/bin/$(ConfigurationName)/lua_scripts/extensions/"
-  )
-  elseif ( MINGW )
-    add_custom_command(TARGET game
-    POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_BINARY_DIR}/bin/lua_scripts/extensions/"
-    COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_MOD_ELUNA_ENGINE_DIR}/LuaEngine/extensions" "${CMAKE_BINARY_DIR}/bin/lua_scripts/extensions/"
-  )
+  if (MSVC)
+    set(MSVC_CONFIGURATION_NAME $(ConfigurationName)/)
   endif()
+  add_custom_command(TARGET game
+  POST_BUILD
+  COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_BINARY_DIR}/bin/${MSVC_CONFIGURATION_NAME}lua_scripts/extensions/"
+  COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_MOD_ELUNA_ENGINE_DIR}/LuaEngine/extensions" "${CMAKE_BINARY_DIR}/bin/${MSVC_CONFIGURATION_NAME}lua_scripts/extensions/"
+ )
 endif()
 
 install(DIRECTORY "${CMAKE_MOD_ELUNA_ENGINE_DIR}/LuaEngine/extensions" DESTINATION "${CMAKE_INSTALL_PREFIX}/bin/lua_scripts/")
