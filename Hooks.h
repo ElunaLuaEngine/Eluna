6c96efe169ff1218adc24cff5e2eed610dae6210
BuildingOutLoud <a_sage_00@hotmail.com>
2021-04-16 13:52:24 -0600

    object, not gameobject

--- Hooks.h
+++ Hooks.h
@@ -204,7 +204,7 @@ namespace Hooks
         // UNUSED                               =     40,       // (event, player)
         // UNUSED                               =     41,       // (event, player)
         PLAYER_EVENT_ON_COMMAND                 =     42,       // (event, player, command) - player is nil if command used from console. Can return false
-        PLAYER_EVENT_ON_QUEST_COMPLETE          =     43,       // (event, player, gameobject, quest)
+        PLAYER_EVENT_ON_QUEST_COMPLETE          =     43,       // (event, player, object, quest)
 
         PLAYER_EVENT_COUNT
     };
