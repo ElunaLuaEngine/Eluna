71e775e84b2371599d36251de2eeb0f132c9d03d
iThorgrim.exe <raindash@outlook.fr>
2021-08-02 20:38:35 +0200

    Update LuaEngine.h

--- LuaEngine.h
+++ LuaEngine.h
@@ -360,6 +360,7 @@ public:
     void OnResurrect(Player* pPlayer);
     void OnQuestAbandon(Player* pPlayer, uint32 questId);
     void OnLearnTalents(Player* pPlayer, uint32 talentId, uint32 talentRank, uint32 spellid);
+    void OnLearnSpell(Player* pPlayer, uint32 spellid);
     InventoryResult OnCanUseItem(const Player* pPlayer, uint32 itemEntry);
     void OnLuaStateClose();
     void OnLuaStateOpen();
