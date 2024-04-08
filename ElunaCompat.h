c2bf6da5025bc27384c9f4abfd1105426627b129
Rochet2 <tqbattlenet@gmail.com>
2024-04-08 21:21:48 +0300

    Remove semicolons from macros

--- ElunaCompat.h
+++ ElunaCompat.h
@@ -19,19 +19,19 @@ extern "C"
     const char* luaL_tolstring(lua_State* L, int idx, size_t* len);
     int lua_absindex(lua_State* L, int i);
     #define lua_pushglobaltable(L) \
-        lua_pushvalue((L), LUA_GLOBALSINDEX);
+        lua_pushvalue((L), LUA_GLOBALSINDEX)
     #define lua_rawlen(L, idx) \
-        lua_objlen(L, idx);
+        lua_objlen(L, idx)
     #define lua_pushunsigned(L, u) \
-        lua_pushinteger(L, u);
+        lua_pushinteger(L, u)
     #define lua_load(L, buf_read, dec_buf, str, NULL) \
-        lua_load(L, buf_read, dec_buf, str);
+        lua_load(L, buf_read, dec_buf, str)
 #endif
 
 #if LUA_VERSION_NUM > 502
     #define lua_dump(L, writer, data) \
-        lua_dump(L, writer, data, 0);
+        lua_dump(L, writer, data, 0)
     #define lua_pushunsigned(L, u) \
-        lua_pushinteger(L, u);
+        lua_pushinteger(L, u)
 #endif
 #endif
