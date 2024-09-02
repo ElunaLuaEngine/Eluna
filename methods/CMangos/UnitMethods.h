6bf4862ac2c1bf91f6f2d33e17aad25d2c645d26
Niam5 <ebakkedahl@gmail.com>
2024-09-02 12:33:44 -0700

    Add stat modifier unit methods for CMaNGOS
    Co-Authored-By: Foe <Foereaper@users.noreply.github.com>

--- methods/CMangos/UnitMethods.h
+++ methods/CMangos/UnitMethods.h
@@ -2417,6 +2417,71 @@ namespace LuaUnit
         return 0;
     }
 
+    /**
+    * Returns whether or not the [Unit] can have stat modifiers applied.
+    *
+    * @return bool canModifyStats
+    */
+    int CanModifyStats(Eluna* E, Unit* unit)
+    {
+        E->Push(unit->CanModifyStats());
+        return 1;
+    }
+
+    /**
+    * Modifies a flat amount of a specific stat of the [Unit]
+    *
+    * <pre>
+    * enum UnitModifierFlatType
+    * {
+    *      BASE_VALUE = 0,
+    *      TOTAL_VALUE = 1
+    * };
+    * </pre>
+    *
+    * @param uint32 statType : The stat to modify
+    * @param [UnitModifierFlatType] modType : The type of modifier to apply
+    * @param float value : The value to apply to the stat
+    * @param bool apply = true : True applies a positive modifier, false applies a negative
+    */
+    int AddFlatStatModifier(Eluna* E, Unit* unit)
+    {
+        uint32 statType = E->CHECKVAL<uint32>(2);
+        uint8 modType = E->CHECKVAL<uint8>(3);
+        float value = E->CHECKVAL<float>(4);
+        bool apply = E->CHECKVAL<bool>(5, true);
+        UnitModifierType type = (modType == 0) ? BASE_VALUE : TOTAL_VALUE;
+
+        unit->HandleStatModifier(UnitMods(UNIT_MOD_STAT_START + statType), (UnitModifierType)type, value, apply);
+        return 0;
+    }
+
+    /**
+    * Modifies a percentage amount of a specific stat of the [Unit]
+    *
+    * <pre>
+    * enum UnitModifierPctType
+    * {
+    *      BASE_PCT = 0,
+    *      TOTAL_PCT = 1
+    * };
+    * </pre>
+    *
+    * @param uint32 statType : The stat to modify
+    * @param [UnitModifierPctType] modType : The type of modifier to apply
+    * @param float value : The value to apply to the stat
+    */
+    int AddPctStatModifier(Eluna* E, Unit* unit)
+    {
+        uint32 statType = E->CHECKVAL<uint32>(2);
+        uint8 modType = E->CHECKVAL<uint8>(3);
+        float value = E->CHECKVAL<float>(4);
+        UnitModifierType type = (modType == 0) ? BASE_PCT : TOTAL_PCT;
+
+        unit->HandleStatModifier(UnitMods(UNIT_MOD_STAT_START + statType), (UnitModifierType)type, value, true);
+        return 0;
+    }
+
     ElunaRegister<Unit> UnitMethods[] =
     {
         // Getters
@@ -2528,6 +2593,7 @@ namespace LuaUnit
         { "HasAura", &LuaUnit::HasAura },
         { "IsCasting", &LuaUnit::IsCasting },
         { "IsStandState", &LuaUnit::IsStandState },
+        { "CanModifyStats", &LuaUnit::CanModifyStats },
 
         // Other
         { "AddAura", &LuaUnit::AddAura },
@@ -2568,6 +2634,8 @@ namespace LuaUnit
         { "MoveClear", &LuaUnit::MoveClear },
         { "DealDamage", &LuaUnit::DealDamage },
         { "DealHeal", &LuaUnit::DealHeal },
+        { "AddFlatStatModifier", &LuaUnit::AddFlatStatModifier },
+        { "AddPctStatModifier", &LuaUnit::AddPctStatModifier },
 
         // Expansion specific methods
 #if ELUNA_EXPANSION >= EXP_TBC
