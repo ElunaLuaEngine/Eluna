ee9d93660fdefb88a0ebd9d5c82d762e32f9d206
Foereaper <foereaper@elunatech.com>
2024-09-25 21:09:48 +0200

    Add dark mode theme

---
+++ docs/ElunaDoc/static/theme.js
@@ -0,0 +1,18 @@
+const themeToggle = document.getElementById('themeToggle');
+
+// Check if user preference exists in local storage
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.classList.add(currentTheme);
+}
+
+themeToggle.addEventListener('click', () => {
+    document.documentElement.classList.toggle('dark-mode');
+
+    // Store user preference in local storage
+    if (document.documentElement.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        localStorage.setItem('theme', '');
+    }
+});
