58d7652138887783228b9727bd1fa4e08f00342b
tb <tbgitemail@gmail.com>
2026-07-11 22:54:53 -0400

    Fix TC Build.

--- methods/TrinityCore/PlayerMethods.h
+++ methods/TrinityCore/PlayerMethods.h
@@ -2543,7 +2543,7 @@ namespace LuaPlayer
      */
     int ResetTalentsCost(Eluna* E, Player* player)
     {
-        E->Push(player->ResetTalentsCost());
+        E->Push(player->GetNextResetTalentsCost());
         return 1;
     }
 
